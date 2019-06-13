f646bdb767f69967bf04171e13111a1234ef4915
Eliot Howes <ejhowes@gmail.com>
2019-06-13 17:31:38 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,14 @@
+require 'pry'
+
 def consolidate_cart(cart)
   # code here
+
+  cart.each do |hash|
+    hash.each do |key, value|
+      hash[key][:count] = 1
+    end
+  end
+  cart
 end
 
 def apply_coupons(cart, coupons)
