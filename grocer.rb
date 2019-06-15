7bd88b83b334fe780c6c7913e61f0e0c509e3b5f
Eliot Howes <ejhowes@gmail.com>
2019-06-15 20:45:01 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -19,23 +19,59 @@ end
 def apply_coupons(cart, coupons)
   # code here
 
-  coupons.each do |discount_product|
-    if discount_product[:item] == cart.each_with_index do |product, index|
-      product[1][:count] -= discount_product[:num]
-      end
+  # result = {}
+  # # code here#
+  # cart.each do |food, info|
+  #   coupons.each do |coupon|
+  #     if food == coupon[:item] && info[:count] >= coupon[:num]
+  #       info[:count] =  info[:count] - coupon[:num]
+  #       if result["#{food} W/COUPON"]
+  #         result["#{food} W/COUPON"][:count] += 1
+  #       else
+  #         result["#{food} W/COUPON"] = {:price => coupon[:cost], :clearance => info[:clearance], :count => 1}
+  #       end
+  #     end
+  #   end
+  #   result[food] = info
+  # end
+  # result
+
+cart_with_coupons = {}
+
+cart.each do |food, info|
+  coupons.each do |coupon|
+    if food == coupon[:item] && info[:count] >= coupon[:num]
+      item_count = info[:count] = info[:count] - coupon[:num]
+      cart_with_coupons[food] = info
+      cart_with_coupons[food + " W/COUPON"] = {:price => (coupon[:cost] / coupon[:num]) , :clearance => true, :count => item_count}
+    else
+      cart_with_coupons[food] = info
     end
   end
-  cart
 end
-
-# discount_product = {:item=>"AVOCADO", :num=>2, :cost=>5.0}
-# product = ["AVOCADO", {:price=>3.0, :clearance=>true, :count=>2}]
-
+cart_with_coupons
+end
 
 def apply_clearance(cart)
   # code here
+  clearance_cart = {}
+
+  cart.each do |food, info|
+    if info[:clearance] == true
+      clearance_cart[food] = {:price => ((info[:price] / 5.00) * 4.00), :clearance => info[:clearace], :count => info[:count]}
+    else
+      clearance_cart[food] = info
+    end
+  end
+  clearance_cart
 end
 
 def checkout(cart, coupons)
   # code here
+  cart_total = 0
+
+  consolidate_cart(cart).each do |food, info|
+    cart_total += info[:price]
+  end
+  cart_total
 end
